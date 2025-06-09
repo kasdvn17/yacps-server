ea092ef10629426f6d76068eef4175cf9c2afd8a
kasdvn17 <kasdvn.yt@aol.com>
2025-06-09 14:22:00 +0700

    feat: enable all domain cors for development

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(port).then(() => console.log(`Listening on port ${port}`));
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
