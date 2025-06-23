9ff86a5b636a834e73d8f5b6279075d0a5e8e76b
kasdvn17 <77109479+kasdvn17@users.noreply.github.com>
2025-06-23 22:57:41 +0700

    feat: template judge api ws

---
+++ src/judge-api/auth/auth.guard.ts
@@ -0,0 +1,44 @@
+import { PrismaService } from '@/prisma/prisma.service';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { WsException } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+
+@Injectable()
+export class JAuthGuard implements CanActivate {
+  private readonly logger = new Logger(JAuthGuard.name);
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const client: Socket = context.switchToWs().getClient();
+    console.log(client.id);
+    const token = client.handshake.auth.token;
+    if (!token) throw new WsException('INVALID_TOKEN');
+    try {
+      const payload = await this.jwtService.verifyAsync(token); // payload.id, payload.name, payload.createdAt
+      const judge = await this.prismaService.judge.findFirst({
+        where: {
+          token: {
+            id: payload.id,
+            createdAt: payload.createdAt,
+          },
+        },
+      });
+      if (!judge) throw new WsException('INVALID_TOKEN');
+      if (judge.status != 'ACTIVE') throw new WsException('JUDGE_DISALBED');
+      client.data['judge'] = judge;
+      return true;
+    } catch (err) {
+      this.logger.error(err);
+      throw new WsException('UNKNOWN_ERROR');
+    }
+  }
+}
