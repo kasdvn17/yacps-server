1fedc487ac44e6ac3e612ccaa35c5779c1a9a0d1
kasdvn17 <lam@blam.id.vn>
2025-06-24 21:15:59 +0700

    fix: sessions expiry token

--- src/client-api/auth/auth.module.ts
+++ src/client-api/auth/auth.module.ts
@@ -1,10 +1,24 @@
 import { Global, Module } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
+import { JwtModule } from '@nestjs/jwt';
+import { Config } from 'config';
 
 @Global()
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_CLIENT_TOKEN,
+      signOptions: { expiresIn: Config.SESSION_EXPIRES_MS / 1000 },
+    }),
+  ],
   controllers: [],
   providers: [PermissionsService],
-  exports: [PermissionsService],
+  exports: [
+    PermissionsService,
+    JwtModule.register({
+      secret: process.env.JWT_CLIENT_TOKEN,
+      signOptions: { expiresIn: Config.SESSION_EXPIRES_MS / 1000 },
+    }),
+  ],
 })
 export class AuthModule {}
