638d51796906cb38463cecedca8c5f5c8136d54f
kasdvn17 <lam@blam.id.vn>
2025-06-28 12:11:26 +0700

    feat: enable captcha by default

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export const Config = {
   ENABLE_USER_SELF_REGISTRATIONS: true, // Allow users to self-register, otherwise, admins must manually create users, default: true
   ENABLE_MAIL_CONFIRMATION: false, // Requires users to confirm registrations via email, default: false
-  ENABLE_CAPTCHA: false, // Require users to complete Turnstile captcha when creating users, sessions - default: true
+  ENABLE_CAPTCHA: true, // Require users to complete Turnstile captcha when creating users, sessions - default: true
   SESSION_EXPIRES_MS: 604800000, // Session expires at duration, default: a week
 
   /* Argon2id configuration */
