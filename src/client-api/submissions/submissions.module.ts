9abc1adc93c1233319a405ac7712f20d95e62d30
kasdvn17 <77109479+kasdvn17@users.noreply.github.com>
2025-08-27 14:15:12 +0700

    feat: activity heatmap route

--- src/client-api/submissions/submissions.module.ts
+++ src/client-api/submissions/submissions.module.ts
@@ -5,10 +5,11 @@ import { JudgeAPIModule } from '@/judge-api/judge-api.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ProblemsService } from '../problems/problems.service';
 import { SubmissionsService } from './submissions.service';
+import { UsersService } from '../users/users.service';
 
 @Module({
   imports: [PrismaModule, JudgeAPIModule, EventEmitterModule],
   controllers: [SubmissionsController],
-  providers: [ProblemsService, SubmissionsService],
+  providers: [ProblemsService, SubmissionsService, UsersService],
 })
 export class SubmissionsModule {}
